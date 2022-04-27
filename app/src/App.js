e3dc7449bf412481629fc7b84fb0e9bc0c1b8253
Toan Luu Vinh <toan.luu@monstar-lab.com>
2022-04-27 08:05:44 +0700

    ReactDay 7 Hooks

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-import Form from "./components/Form";
+import Form from "./components/Form/Form";
+import ListUser from "./components/UserList/ListUser";
 function App() {
   return (
     <div className="App">
       <Form />
+      <ListUser />
     </div>
   );
 }
